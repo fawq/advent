0cccaa9f5c052b506e9490c34541388309fb1689
Krystian Krakowski <kkrakowski22@gmail.com>
2025-07-27 18:14:24 +0200

    Add tests

---
+++ python/advent_2024_01/src/advent_2024_01/test_task_01.py
@@ -0,0 +1,5 @@
+from advent_2024_01.task_01 import day_01_main
+
+
+def test_day_01_main() -> None:
+    assert day_01_main() == 2367773
---
+++ python/advent_2024_01/src/advent_2024_01/test_task_02.py
@@ -0,0 +1,5 @@
+from advent_2024_01.task_02 import day_02_main
+
+
+def test_day_02_main() -> None:
+    assert day_02_main() == 21271939
