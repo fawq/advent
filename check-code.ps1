743f7e2fe5be4b369049cd37ac6e942a5fff747c
Krystian Krakowski <kkrakowski22@gmail.com>
2025-07-28 20:31:52 +0200

    Revert

--- check-code.ps1
+++ check-code.ps1
@@ -6,7 +6,7 @@ Get-ChildItem -Path mixed/ -Recurse -Depth 0 | Where-Object { $_.PSIsContainer }
     cargo --config win_config/win_rust_config.toml run --bin stub_gen 
 }
 Get-ChildItem -Path mixed/ -Recurse -Depth 0 | Where-Object { $_.PSIsContainer } | ForEach-Object { 
-    maturin develop --config win_config/win_rust_config.toml --release --uv -m mixed/$_/Cargo.toml 
+    maturin develop --release --uv -m mixed/$_/Cargo.toml 
 }
 
 # Check all rust code (also mixed workspaces)
