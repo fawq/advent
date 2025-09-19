121f4e69626eb55e6a1f46faaacb6784f27cb483
Krystian Krakowski <kkrakowski22@gmail.com>
2025-09-19 21:19:36 +0200

    Update libraries

--- python/advent_2024_02/src/advent_2024_02/task_01.py
+++ python/advent_2024_02/src/advent_2024_02/task_01.py
@@ -8,12 +8,14 @@ def is_safe(array_number: np.typing.NDArray[np.int8]) -> np.bool:
     diffs = np.diff(array_number)
     return np.all((diffs >= 1) & (diffs <= 3)) or np.all((diffs <= -1) & (diffs >= -3))
 
+
 def count_safe(array_numbers: list[np.typing.NDArray[np.int8]]) -> int:
     counter: int = 0
     for array_number in array_numbers:
         if is_safe(array_number):
             counter += 1
     return counter
+
 
 def day_02_task_01_main() -> int:
     array_numbers = read_lines_to_vec_of_array1d_i8(
--- python/advent_2024_02/src/advent_2024_02/test_task_02.py
+++ python/advent_2024_02/src/advent_2024_02/test_task_02.py
@@ -2,4 +2,9 @@ from advent_2024_02.task_02 import day_02_task_02_main
 
 
 def test_day_02_task_02_main() -> None:
+    """
+    Test the day_02_task_02_main function.
+
+    The function should return 386.
+    """
     assert day_02_task_02_main() == 386
