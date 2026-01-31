55788471e96d0269554d9258f79a7e8eec69b855
Krystian Krakowski <kkrakowski22@gmail.com>
2026-01-31 10:32:41 +0100

    Update repo

--- python/advent_2024_01/src/advent_2024_01/task_02.py
+++ python/advent_2024_01/src/advent_2024_01/task_02.py
@@ -12,7 +12,7 @@ def calculate_similarity_score(array_numbers: np.typing.NDArray[np.int32]) -> in
 
 
 def day_01_task_02_main() -> int:
-    array_numbers = utils.read_lines_to_array2d_u32(
+    array_numbers = utils.read_lines_to_array2d_i32(
         "python/advent_2024_01/data/data.txt"
     )
     return calculate_similarity_score(array_numbers)
